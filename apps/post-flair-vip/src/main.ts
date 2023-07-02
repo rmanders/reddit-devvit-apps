9223f9be163c9c4728dc9e3b4fc262ee265a60c2
Ryan Anderson <ryan.anderson.01@gmail.com>
2023-07-02 17:00:58 -0400

    progress

---
+++ apps/post-flair-vip/src/main.ts
@@ -0,0 +1,60 @@
+import {
+    PostSubmit,
+    Metadata,
+  } from '@devvit/protos';
+
+import { Devvit, RedditAPIClient, getSetting } from '@devvit/public-api';
+
+// TODO: REMOVE THIS and replace with config or dynamically acquired value
+const SUBREDDIT = 'schlockenspiel';
+const SETTING_VIP_FLAIR = 'vip-post-flair';
+
+//reddit.getPostFlairTemplates(SUBREDDIT, )
+
+
+Devvit.addSettings([
+    {
+        type: 'string',
+        name: SETTING_VIP_FLAIR,
+        label: 'Enter Post flair to be VIP-limited'
+    }
+]);
+
+Devvit.addTrigger({
+    event: Devvit.Trigger.PostSubmit,
+    async handler(postSubmission: PostSubmit, metadata?: Metadata) {
+      console.log(`Received OnPostSubmit event:\n${JSON.stringify(postSubmission)}\n`,
+      `Metadata:\n${JSON.stringify(metadata)}`);
+      // TODO: if post is using vip flair, the check user is in vip list. if not, remove
+
+      // REMOVE THIS: list all post flair
+      const reddit = new RedditAPIClient();
+      const sub = await reddit.getSubredditByName(SUBREDDIT, metadata);
+      const postFlairs = await sub.getPostFlairTemplates();
+      console.log(`Post Flairs:\n${JSON.stringify(postFlairs)}`);
+
+
+      // Get the vip flair:
+      const vipFlairText = await getSetting(SETTING_VIP_FLAIR, metadata);
+      console.log(`VIP flair setting: ${vipFlairText}`);
+
+      // Get the current Post flair (if any)
+      const curPostFlair = postSubmission.post?.linkFlair?.text;
+      if (!curPostFlair) {
+        console.log("Not post flair set. Nothing to do");
+        return;
+      }
+      console.log(`Current Post Flair text: ${curPostFlair}`);
+
+      // Check if post flair is vip
+      if (curPostFlair !== vipFlairText) {
+        console.log(`Post Flair is not same as VIP Flair. No further action needed`);
+        return;
+      }
+
+      // Check if user is in VIP list
+
+    },
+  });
+
+export default Devvit;
