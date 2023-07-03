cb6249a434642de6b82f92c0234d0594a29ece2c
Ryan Anderson <ryan.anderson.01@gmail.com>
2023-07-03 17:28:42 -0400

    Refactored for better readability

---
+++ apps/post-flair-vip/src/flairPasslist.ts
@@ -0,0 +1,139 @@
+import { SettingsFormFieldValidatorEvent } from "@devvit/public-api/settings/types.js";
+import {
+    Metadata,
+    PostSubmit,
+    LogMessage,
+    Severity,
+  } from '@devvit/protos';
+import { Devvit, getSetting } from "@devvit/public-api";
+
+const logger = Devvit.use(Devvit.Types.Logger);
+
+// Constants
+export const SETTING_PROTECTED_FLAIR = 'protected-flair-text';
+export const SETTING_PASSLIST_USERS = 'passlist-usernames';
+
+const MAX_PASSLIST_CHARS = 2000;
+const MAX_PROTECTED_FLAIR_CHARS = 64;
+
+export function validatePassList(
+    event: SettingsFormFieldValidatorEvent<string>, 
+    _metadata: Metadata | undefined
+) {
+    if (!event.value) { return; }
+    if (event.value.length > MAX_PASSLIST_CHARS) {
+        return `Pass list must be less than ${MAX_PASSLIST_CHARS} characters`;
+    }
+    // TODO: maybe validate each username is a reddit user, but may take too long on each edit
+}
+
+export function validateProtectedFlair(
+    event: SettingsFormFieldValidatorEvent<string>, 
+    _metadata: Metadata | undefined
+) {
+    if (!event.value) { return; }
+    if (event.value.length > MAX_PROTECTED_FLAIR_CHARS) {
+        return `Flair text must be less than ${MAX_PASSLIST_CHARS} characters`
+    }
+    // TODO: check against the subreddits existing flair
+}
+
+/**
+ * Gets an array of Reddit usernames in the pass-list from the application settings.
+ * If the settings could not be parsed or doesn't exist, undefined is returned.
+ * @param metadata metadata object needed by Devvit
+ * @returns Optional array containing reddit usernames in the pass-list
+ */
+export async function getPassListUsernames(
+    metadata: Metadata
+): Promise<string[]|undefined> {
+
+    const passListText: string|undefined = await getSetting(SETTING_PASSLIST_USERS, metadata);
+    if (passListText == undefined) {
+        console.log(`Not value found for setting: [${SETTING_PASSLIST_USERS}]`);
+        return;
+    }
+
+    const usernames = passListText.split(/\s+/);
+    if (!usernames || usernames.length <= 0) {
+        console.log(`Setting: [${SETTING_PASSLIST_USERS}] contained no useable values: [${passListText}]`);
+        return;
+    }
+    return usernames;
+}
+
+/**
+ * Gets the text value setting of the Link/Post flair that's proctected by the passlist. 
+ * If the setting could not be parsed or doesn't exist, undefined is returned.
+ * @param metadata metadata object needed by Devvit
+ * @returns Optional value of the post flair text that should be protected
+ */
+export async function getProtectedFlairText(
+    metadata: Metadata
+): Promise<string|undefined> {
+    const flairText: string|undefined = await getSetting(SETTING_PROTECTED_FLAIR, metadata);
+    if (flairText == undefined || flairText.trim().length == 0) {
+        console.log(`No useable value found for setting: [${SETTING_PROTECTED_FLAIR}]`);
+        return;
+    }
+    return flairText!.trim();
+}
+
+/**
+ * Decides whether or not the post should be allowed
+ * @param postSubmission devvit object containing post data
+ * @param metadata metadata object needed by Devvit
+ * @returns FlairPassListRequest or undefined
+ */
+export async function shouldPostPass(
+    postSubmission: PostSubmit,
+    metadata: Metadata
+): Promise<boolean> {
+
+    // Get all data needed for the request
+    const protectedFlairText = await getProtectedFlairText(metadata);
+    const passListUserNames = await getPassListUsernames(metadata);
+    const postAuthor = postSubmission.author?.name;
+    const postFlair = postSubmission.post?.linkFlair?.text
+
+    await logI('Flair Pass List Request Details:' +
+        `\nSetting - [${SETTING_PASSLIST_USERS}]: ${passListUserNames}` +
+        `\nSetting - [${SETTING_PROTECTED_FLAIR}]: ${protectedFlairText}` +
+        `\npostAuthor: ${postAuthor}` +
+        `\npostFlairText: ${postFlair}\n\n`, metadata);
+
+    // Check that ALL values are present. If not, we can't process the post
+    if (protectedFlairText == undefined || passListUserNames == undefined || 
+        postAuthor == undefined || postFlair == undefined) {
+            await logI('ALLOW: Unable to get all info needed to process Post Flair passList. Allowing the post', metadata);
+            return true;
+    }
+
+    // If the post flair is set, and it's not protected. Then allow
+    if (postFlair != protectedFlairText) {
+        await logI(`ALLOW: Post flair of [${postFlair}] is not protected. Protected flair: [${protectedFlairText}]`, metadata);
+        return true;
+    }
+
+    // If the post's author is in the pass-list, then allow the post
+    if (passListUserNames.indexOf(postAuthor) > -1) {
+        await logI(`ALLOW: User [${postAuthor}] is in pass-list for protected flair: [${protectedFlairText}]`, metadata);
+        return true;
+    }
+
+    // If we reach this point, post author is NOT in the passlist for the protected post flair
+    await logI(`DENY: User [${postAuthor}] is NOT in pass-list for protected flair: [${protectedFlairText}]`, metadata);
+    return false;
+}
+
+/**
+ * Log an Info message
+ * @param message
+ * @param metadata 
+ */
+async function logI(message: string|undefined, metadata?: Metadata) {
+    await logger.Log(LogMessage.fromPartial({
+        severity: Severity.INFO,
+        message: message
+    }), metadata);
+}
--- apps/post-flair-vip/src/main.ts
+++ apps/post-flair-vip/src/main.ts
@@ -4,26 +4,29 @@ import {
   } from '@devvit/protos';
 
 import { 
-    SETTING_VIP_FLAIR,
-    SETTING_VIP_USERS,
-    validateVipUsers
-} from './validators.js'
+    SETTING_PROTECTED_FLAIR,
+    SETTING_PASSLIST_USERS,
+    validatePassList,
+    validateProtectedFlair,
+    shouldPostPass
+} from './flairPasslist.js'
 
-import { Devvit, RedditAPIClient, getSetting } from '@devvit/public-api';
+import { Devvit, RedditAPIClient } from '@devvit/public-api';
 
 const reddit = new RedditAPIClient();
 
 Devvit.addSettings([
     {
         type: 'string',
-        name: SETTING_VIP_FLAIR,
+        name: SETTING_PROTECTED_FLAIR,
         label: 'The text value of the protected post flair',
+        onValidate: validateProtectedFlair
     },
     {
         type: 'paragraph',
-        name: SETTING_VIP_USERS,
+        name: SETTING_PASSLIST_USERS,
         label: 'Reddit usernames of users in the pass-list (without the "u/", one per line)',
-        onValidate: (event, metadata) => { validateVipUsers(event, metadata, reddit); }
+        onValidate: validatePassList
     }
 ]);
 
@@ -37,52 +40,10 @@ Devvit.addTrigger({
       console.log(`Received OnPostSubmit event:\n${JSON.stringify(postSubmission)}\n`,
       `Metadata:\n${JSON.stringify(metadata)}\n\n`);
 
-      // Get the vip flair:
-      const vipFlairText = await getSetting(SETTING_VIP_FLAIR, metadata);
-      console.log(`VIP flair setting: ${vipFlairText}`);
-
-      // Get the current Post flair (if any)
-      const curPostFlair = postSubmission.post?.linkFlair?.text;
-      if (!curPostFlair) {
-        console.log("No post flair set. Nothing to do");
-        return;
+      // Check if the post should be allowed based on the flair and author
+      if (!await shouldPostPass(postSubmission, metadata!)) {
+        await reddit.remove(postSubmission.post!.id, false, metadata);
       }
-      console.log(`Current Post Flair text: ${curPostFlair}`);
-
-      // Check if post flair is vip
-      if (curPostFlair !== vipFlairText) {
-        console.log(`Post Flair is not same as VIP Flair. No further action needed`);
-        return;
-      }
-      console.log(`Post's flair is a VIP flair.`);
-
-      // Check if user is in VIP list
-      const author = postSubmission.author?.name;
-      if (!author) {
-        console.log('Can\'t get post author. Exiting.');
-        return;
-      }
-      console.log(`Found post author: ${author}`);
-
-      // Get the list if VIP users
-      const vipUsersText: String|undefined = await getSetting(SETTING_VIP_USERS, metadata);
-      if (!vipUsersText) {
-        console.log(`${SETTING_VIP_USERS} settings value is empty. Exiting.`);
-        return;
-      }
-      const vipUsers = vipUsersText.split(/\s+/);
-
-      // Check if post author is in VIP list
-      if (vipUsers.indexOf(author) > -1) {
-        console.log(`Author: ${author}, is a VIP user for flair: ${vipFlairText}!`);
-        return;
-      }
-      
-      // If author is not a VIP, remove the post
-      // TODO: Can we add a removal reason or detail for the mod log?
-      console.log(`Author: ${author}, is NOT VIP user for flair: ${vipFlairText}. Removing post!`);
-      await reddit.remove(postSubmission.post!.id, false, metadata);
-
     },
   });
 
