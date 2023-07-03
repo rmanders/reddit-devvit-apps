72c511f0da105c78cc4d5284860c82738ec812e9
Ryan Anderson <ryan.anderson.01@gmail.com>
2023-07-03 13:45:02 -0400

    Added settings helper functions

--- apps/post-flair-vip/src/main.ts
+++ apps/post-flair-vip/src/main.ts
@@ -3,13 +3,13 @@ import {
     Metadata,
   } from '@devvit/protos';
 
-  import { validateVipUsers } from './validators.js'
+import { 
+    SETTING_VIP_FLAIR,
+    SETTING_VIP_USERS,
+    validateVipUsers
+} from './validators.js'
 
 import { Devvit, RedditAPIClient, getSetting } from '@devvit/public-api';
-
-// TODO: REMOVE THIS and replace with config or dynamically acquired value
-const SETTING_VIP_FLAIR = 'vip-post-flair';
-const SETTING_VIP_USERS = 'vip_users';
 
 const reddit = new RedditAPIClient();
 
@@ -17,29 +17,25 @@ Devvit.addSettings([
     {
         type: 'string',
         name: SETTING_VIP_FLAIR,
-        label: 'Existing post flair text that should be VIP-limited',
+        label: 'The text value of the protected post flair',
     },
     {
         type: 'paragraph',
         name: SETTING_VIP_USERS,
-        label: 'VIP users (one per line)',
+        label: 'Reddit usernames of users in the pass-list (without the "u/", one per line)',
         onValidate: (event, metadata) => { validateVipUsers(event, metadata, reddit); }
     }
 ]);
 
+/**
+ * Trigger that checks on each new post
+ */
 Devvit.addTrigger({
     event: Devvit.Trigger.PostSubmit,
     async handler(postSubmission: PostSubmit, metadata?: Metadata) {
-
+        
       console.log(`Received OnPostSubmit event:\n${JSON.stringify(postSubmission)}\n`,
       `Metadata:\n${JSON.stringify(metadata)}\n\n`);
-
-      // REMOVE THIS: TODO: How do we get available post flairs for a subreddit?
-      console.log('Getting subreddit...');
-      const sub = await reddit.getCurrentSubreddit(metadata);
-      console.log('Getting flair templates');
-      const postFlairs = await sub.getPostFlairTemplates();
-      console.log(`Post Flairs:\n${JSON.stringify(postFlairs)}`);
 
       // Get the vip flair:
       const vipFlairText = await getSetting(SETTING_VIP_FLAIR, metadata);
@@ -48,7 +44,7 @@ Devvit.addTrigger({
       // Get the current Post flair (if any)
       const curPostFlair = postSubmission.post?.linkFlair?.text;
       if (!curPostFlair) {
-        console.log("Not post flair set. Nothing to do");
+        console.log("No post flair set. Nothing to do");
         return;
       }
       console.log(`Current Post Flair text: ${curPostFlair}`);
--- apps/post-flair-vip/src/validators.ts
+++ apps/post-flair-vip/src/validators.ts
@@ -2,7 +2,10 @@ import { SettingsFormFieldValidatorEvent } from "@devvit/public-api/settings/typ
 import {
     Metadata,
   } from '@devvit/protos';
-import { RedditAPIClient } from "@devvit/public-api";
+import { RedditAPIClient, getSetting } from "@devvit/public-api";
+
+export const SETTING_VIP_FLAIR = 'vip-post-flair';
+export const SETTING_VIP_USERS = 'vip_users';
 
 export async function validateVipUsers(
     event: SettingsFormFieldValidatorEvent<string>, 
@@ -18,4 +21,33 @@ export async function validateVipUsers(
             return `Invalid reddit username: ${name}`
         }
     }
+}
+
+export async function getPassListUsernames(
+    metadata: Metadata
+): Promise<string[]|undefined> {
+
+    const passListText: string|undefined = await getSetting(SETTING_VIP_USERS, metadata);
+    if (passListText == undefined) {
+        console.log(`Not value found for setting: [${SETTING_VIP_USERS}]`);
+        return;
+    }
+
+    const usernames = passListText.split(/\s+/);
+    if (!usernames || usernames.length <= 0) {
+        console.log(`Setting: [${SETTING_VIP_USERS}] contained no useable values: [${passListText}]`);
+        return;
+    }
+    return usernames;
+}
+
+export async function getProtectedFlairText(
+    metadata: Metadata
+): Promise<string|undefined> {
+    const flairText: string|undefined = await getSetting(SETTING_VIP_FLAIR, metadata);
+    if (flairText == undefined || flairText.trim().length == 0) {
+        console.log(`No useable value found for setting: [${SETTING_VIP_FLAIR}]`);
+        return;
+    }
+    return flairText!.trim();
 }
